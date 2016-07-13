00e4ec7166432ef5089c92f90bf0c6c6fbe7d225
feuerblitz7 <alexrasch@gmail.com>
2016-07-13 22:40:05 +0200

    Update logger.c

--- logger.c
+++ logger.c
@@ -164,7 +164,7 @@ int createLogFile(void)
 	struct tm* now_tm;
 	now_tm = localtime(&now);
 
-	strftime(filename, 64, "/home/pi/data/%Y-%m-%d %H:%M.csv", now_tm);
+	strftime(filename, 64, "/home/pi/data/%Y-%m-%D %H:%M.csv", now_tm);
 	
 	// Open file (create if not existing, otherwise truncate)
 	file = fopen(filename, "w");
