4cfd21eec971a04bfd76a8c9f6fa9ad78a900eb2
feuerblitz7 <alexrasch@gmail.com>
2016-07-19 13:59:41 +0200

    Update logger.c

--- logger.c
+++ logger.c
@@ -1,9 +1,11 @@
 // Header
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
 #include <wiringPi.h>
+#include <phidget21.h>
 #include "mcp3008.h"
 
 // Define standards
@@ -37,12 +39,22 @@
 #define ADC_CHAN_POT		0
 #define ADC_CHAN_DCM		1
 
+// Timing
+#define DELAY_MS		8	// Time to wait after sampling
+#define DELAY_IDLE		100
+#define DELAY_BTN		1000
+
 // Variables
 int state, prev_state, line_index;
 int bike = BIKE_ETRIKE;
 int file_transfer_done = FALSE;
+
 struct timeval tval_before, tval_after, tval_result;
+
 FILE *file;
+
+CPhidgetHandle spatial;
+int initSpatial = FALSE;
 
 // Main method
 int main(void)
@@ -62,7 +74,8 @@ int main(void)
 		switch(state)
 		{
 			case STATE_IDLE:
-printf("In Idle state.\n");
+				printf("In Idle state.\n");
+				
 				// At state change
 				if(state != prev_state)
 				{
@@ -78,9 +91,11 @@ printf("In Idle state.\n");
 				{
 					// Change to logging state
 					state = STATE_LOG;
-printf("Switching to logging state!\n");					
+					
+					printf("Switching to logging state!\n");	
+					
 					// Wait 2 s for user to release button
-					delay(2000);
+					delay(DELAY_BTN);
 				}
 				// Check if shutdown button is pressed
 				else if(digitalRead(PIN_BTN_SHUTDOWN) == 0)
@@ -95,23 +110,26 @@ printf("Switching to logging state!\n");
 				else
 				{
 					// By default, delay 100 ms
-					delay(100);
+					delay(DELAY_IDLE);
 				}
-				
+
 				break;
-			
+
 			case STATE_LOG:
 				// At state change
 				if(state != prev_state)
 				{
-printf("Logging just started...\n");
+					printf("Logging just started...\n");
+					
 					// Turn off red LED and turn on green LED
 					digitalWrite(PIN_LED_GREEN, LOW);
 					digitalWrite(PIN_LED_RED, HIGH);
-					
+
 					// Init logging
 					createLogFile();
-printf("Log file created!\n");					
+					
+					printf("Log file created!\n");
+					
 					// Set previous state
 					prev_state = state;
 				}
@@ -120,57 +138,62 @@ printf("Log file created!\n");
 				{
 					// End log file
 					endLogFile();
-					
+
 					// Change back to idle state
 					state = STATE_IDLE;
-					
+
 					// Mark file as unsafed
 					file_transfer_done = FALSE;
-					
+
 					// Wait 2 s for user to release button
-					delay(2000);
+					delay(DELAY_BTN);
 				}
-				
-				// Log data
-				dataLog();
-				
-				// Delay 10 ms to achieve 100 Hz sampling frequency -> TO IMPROVE!
-				delay(10);
+
+				delay(DELAY_MS);
 				break;
-			
+
 			case STATE_FILETRANSFER:
 				// Turn on also red LED
 				digitalWrite(PIN_LED_RED, HIGH);
-printf("File transfer start!\n");
+				
+				printf("File transfer start.\n");
+				
 				// Copy/Move folder from /home/pi/trike/data/ to /media/usb/data
 				moveDataToUSB();
-printf("File transfer done!\n");
+				
+				printf("File transfer done.\n");
+				
 				// Turn off red LED
 				digitalWrite(PIN_LED_RED, LOW);
 
 				// Mark transfer as done
 				file_transfer_done = TRUE;
-				
+
 				// Change back to idle
 				state = STATE_IDLE;
 				break;
-			
+
 			case STATE_SHUTDOWN:
-printf("Going to shutdown!\n");
-digitalWrite(PIN_LED_RED, HIGH);
-delay(250);
-digitalWrite(PIN_LED_RED, LOW);
-delay(250);
-digitalWrite(PIN_LED_RED, HIGH);
-delay(250);
-digitalWrite(PIN_LED_RED, LOW);
-delay(250);
-digitalWrite(PIN_LED_RED, HIGH);
+				// Blink 3 times as sign for shutdown
+				
+				printf("Going to shutdown...\n");
+				
+				digitalWrite(PIN_LED_RED, HIGH);
+				delay(250);
+				digitalWrite(PIN_LED_RED, LOW);
+				delay(250);
+				digitalWrite(PIN_LED_RED, HIGH);
+				delay(250);
+				digitalWrite(PIN_LED_RED, LOW);
+				delay(250);
+				digitalWrite(PIN_LED_RED, HIGH);
+				
+				closeSpatialIMU();
 
 				// Shutdown Pi
 				system("sudo shutdown -h now");
 				break;
-				
+
 			default:
 				break;
 		}
@@ -187,65 +210,47 @@ int createLogFile(void)
 	now_tm = localtime(&now);
 
 	strftime(filename, 64, "/home/pi/data/%Y-%m-%d_%H-%M.csv", now_tm);
-	
+
 	// Open file (create if not existing, otherwise truncate)
 	file = fopen(filename, "w");
-	
+
 	// Write csv header
-	fprintf(file, "Time [s],Steer-angle [deg],Speed [V],Acc-X [g],Acc-Y [g],Acc-Z [g],Rollrate-X [deg/s],Rollrate-Y [deg/s],Rollrate-Z [deg/s]\n");
+	fprintf(file, "Time [s],Steer-angle [LSB],Speed [LSB],Acc-X [g],Acc-Y [g],Acc-Z [g],Rollrate-X [deg/s],Rollrate-Y [deg/s],Rollrate-Z [deg/s]\n");
 
 	// Reset line index
 	line_index = 0;
+
 	// Setup ADC
 	spiSetup(0);
+
+	// Setup IMU
+	if(!initSpatial)
+	{
+		initSpatialIMU();
+		initSpatial = TRUE;
+	}
 }
+
 int endLogFile(void)
 {
+	// Close spatial IMU
+	//closeSpatialIMU();
+
 	// Close csv file
 	fclose(file);
 }
 
-int dataLog(void)
-{
-	// Log time [ms]
-	if(line_index == 0)
-	{
-		// First entry is 0.000000 [s]
-		fprintf(file, "0.000000,");
-
-		// Start time
-		gettimeofday(&tval_before, NULL);
-	}
-	else
-	{
-		// Measure new time and calculate elapsed time
-		gettimeofday(&tval_after, NULL);
-		timersub(&tval_after, &tval_before, &tval_result);
-
-		fprintf(file, "%ld.%06ld,", (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
-	}
-
-	// Log data from ADC
-	logADCData(file);
-
-	// Log data from IMU
-	fprintf(file, "0,0,0,0,0,0\n");
-
-	// Increment line index
-	line_index++;
-}
-
-int setupGPIO(int gpioPin)
+int setupGPIO()
 {
 	// Setup wiringPi GPIO
 	wiringPiSetupGpio();
-	
+
 	// Assign output pins
 	pinMode(PIN_LED_RED, OUTPUT);
 	pinMode(PIN_LED_GREEN, OUTPUT);
 	pinMode(PIN_LED_BTN_LOG, OUTPUT);
-	
-	// Assign input pins 
+
+	// Assign input pins
 	pinMode(PIN_BTN_LOG, INPUT);
 	pullUpDnControl(PIN_BTN_LOG, PUD_UP);
 	pinMode(PIN_BTN_SHUTDOWN, INPUT);
@@ -256,23 +261,171 @@ int setupGPIO(int gpioPin)
 
 int moveDataToUSB()
 {
-	switch(bike)
+	// Count number of files in data
+	int file_count = 0;
+	DIR *dirp;
+	struct dirent *entry;
+	dirp = opendir("/home/pi/data");
+	while((entry = readdir(dirp)) != NULL)
 	{
-		case BIKE_ETRIKE:
-			system("sudo mv /home/pi/data/*.csv /media/usb/data/etrike/");
-			break;
-			
-		case BIKE_EBIKE:
-			system("sudo mv /home/pi/data/*.csv /media/usb/data/ebike/");
-			break;
-			
-		case BIKE_CONVBIKE:
-			system("sudo mv /home/pi/data/*.csv /media/usb/data/convbike/");
-			break;
-			
-		default:
-			break;
+		if(entry->d_type == DT_REG)
+		{
+			file_count++;
+		}
+	}
+
+	// Check if files available for transfer
+	if(file_count > 0)
+	{
+		switch(bike)
+		{
+			case BIKE_ETRIKE:
+				system("sudo mv /home/pi/data/*.csv /media/usb/data/etrike/");
+				break;
+				
+			case BIKE_EBIKE:
+				system("sudo mv /home/pi/data/*.csv /media/usb/data/ebike/");
+				break;
+				
+			case BIKE_CONVBIKE:
+				system("sudo mv /home/pi/data/*.csv /media/usb/data/convbike/");
+				break;
+				
+			default:
+				break;
+		}
+		
+		printf("%d files have been moved.\n", file_count);
 	}
 
 	return 0;
 }
+
+
+//callback that will run if the Spatial is attached to the computer*/
+int CCONV AttachHandler(CPhidgetHandle spatial, void *userptr)
+{
+	int serialNo;
+	CPhidget_getSerialNumber(spatial, &serialNo);
+	printf("Spatial %10d attached!", serialNo);
+
+	return 0;
+}
+
+//callback that will run if the Spatial is detached from the computer
+int CCONV DetachHandler(CPhidgetHandle spatial, void *userptr)
+{
+	int serialNo;
+	CPhidget_getSerialNumber(spatial, &serialNo);
+	printf("Spatial %10d detached! \n", serialNo);
+
+	return 0;
+}
+
+//callback that will run if the Spatial generates an error
+int CCONV ErrorHandler(CPhidgetHandle spatial, void *userptr, int ErrorCode, const char *unknown)
+{
+	printf("Error handled. %d - %s \n", ErrorCode, unknown);
+	return 0;
+}
+
+int CCONV SpatialDataHandler(CPhidgetSpatialHandle spatial, void *userptr, CPhidgetSpatial_SpatialEventDataHandle *data, int count)
+{
+	if(state == STATE_IDLE)
+	{
+		return 0;
+	}
+
+	
+	// Log time [ms], jump over first samples
+	if(line_index == 10)
+	{
+		// First entry is 0.000000 [s]
+		fprintf(file, "0.000000,");
+
+		// Start time
+		gettimeofday(&tval_before, NULL);
+	}
+	else if(line_index > 10)
+	{
+		// Measure new time and calculate elapsed time
+		gettimeofday(&tval_after, NULL);
+		timersub(&tval_after, &tval_before, &tval_result);
+
+		fprintf(file, "%ld.%06ld,", (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
+	}
+
+	if(line_index >= 10)
+	{
+		// Log data from ADC
+		logADCData(file);
+
+		// Log data from IMU
+		int i;
+		for(i = 0; i < count; i++)
+		{
+			fprintf(file, "%6f,%6f,%6f,%6f,%6f,%6f\n", data[i]->acceleration[0], data[i]->acceleration[1], data[i]->acceleration[2], data[i]->angularRate[0], data[i]->angularRate[1], data[i]->angularRate[2]);
+		}
+	}
+
+	// Increment line index
+	line_index++;
+
+	return 0;
+}
+
+int initSpatialIMU()
+{
+	int result;
+	const char *err;
+
+	//Declare a spatial handle
+	CPhidgetSpatialHandle spatial = 0;
+
+	//create the spatial object
+	CPhidgetSpatial_create(&spatial);
+
+	//Set the handlers to be run when the device is plugged in or opened from software, unplugged or closed from software, or generates an error.
+	CPhidget_set_OnAttach_Handler((CPhidgetHandle)spatial, AttachHandler, NULL);
+	CPhidget_set_OnDetach_Handler((CPhidgetHandle)spatial, DetachHandler, NULL);
+	CPhidget_set_OnError_Handler((CPhidgetHandle)spatial, ErrorHandler, NULL);
+
+	//Registers a callback that will run according to the set data rate that will return the spatial data changes
+	//Requires the handle for the Spatial, the callback handler function that will be called, 
+	//and an arbitrary pointer that will be supplied to the callback function (may be NULL)
+	CPhidgetSpatial_set_OnSpatialData_Handler(spatial, SpatialDataHandler, NULL);
+
+	//open the spatial object for device connections
+	CPhidget_open((CPhidgetHandle)spatial, -1);
+
+	//get the program to wait for a spatial device to be attached
+	printf("Waiting for spatial to be attached.... \n");
+	if((result = CPhidget_waitForAttachment((CPhidgetHandle)spatial, 10000)))
+	{
+		CPhidget_getErrorDescription(result, &err);
+		printf("Problem waiting for attachment: %s\n", err);
+		return 0;
+	}
+	
+	//read spatial event data
+	printf("Reading.....\n");
+
+	//Set the data rate for the spatial events
+	CPhidgetSpatial_setDataRate(spatial, 8);
+
+	//run until user input is read
+	printf("Press any key to end\n");
+	//getchar();
+
+	//since user input has been read, this is a signal to terminate the program so we will close the phidget and delete the object we c
+	return 0;
+}
+
+int closeSpatialIMU()
+{
+	CPhidget_close((CPhidgetHandle)spatial);
+	CPhidget_delete((CPhidgetHandle)spatial);
+
+	return 0;
+}
+
